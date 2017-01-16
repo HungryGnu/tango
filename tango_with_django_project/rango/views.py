5b4b19c5568d66dabe55c9e8c007f5cce38c3c16
Ted Johnson <tjohnson@localhost.localdomain>
2017-01-15 22:04:23 -0700

    Added code from chapter 4

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    #construct a dictionary to pass to the tempalge engine as its context
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    #return a redered response to send to the client.
+    # We Make use of the shortcut fucntion to make our lived easier.
+    #Note that the first parameter is the tempale we wish to use
+
+    #return HttpResponse("Rango says hey there partner!")
+    return render(request, 'rango/index.html', context = context_dict)
